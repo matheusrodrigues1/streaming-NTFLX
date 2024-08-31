77966246e9b0e1fd1a9e2d945e1ef46965c48464
matheusrodrigues1 <matech00@outlook.com>
2024-08-30 21:24:52 -0300

    Fetching tv From API

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", movieRoutes);
+app.use("/api/v1/tv", tvRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
