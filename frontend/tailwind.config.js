3608f53dbf28c2f970a16668c0282b4c3b130cc8
matheusrodrigues1 <matech00@outlook.com>
2024-09-05 16:29:10 -0300

    building the home screen

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,10 @@
+import tailwindScrollbarHide from "tailwind-scrollbar-hide";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [tailwindScrollbarHide],
 };
